1a8bafa0437d5cac10cdcf9230e094c6ff433a09
Daniel-Alencar <danielalencar746@gmail.com>
2022-08-30 15:04:21 -0300

    upgrade

--- src/config/smtp.js
+++ src/config/smtp.js
@@ -12,13 +12,10 @@ module.exports = {
 /**
  * REMINDER:
  
- O email que é utilizado neste arquivo é o que será utilizado para fazer o envio para outras pessoas. E, precisamos tanto do email (campo 'user'), como a senha (campo 'pass').
+ O email que é utilizado neste arquivo é o que será utilizado para fazer o envio para outras pessoas.
+ Precisamos tanto do email (campo 'user') como a senha (campo 'pass').
 
  Como as configurações feitas foram muito simples, o gmail por exemplo "acha" que a tecnologia de login é menos segura.
-
- De acordo com o Google:
-
- "Alguns apps e dispositivos usam tecnologias de login menos seguras, o que deixa sua conta vulnerável. Você pode desativar o acesso desses apps, o que recomendamos, ou ativá-lo se optar por usá-los apesar dos riscos."
- Para fazer esta configuração, acesse <https://myaccount.google.com/lesssecureapps>
+ Para mudar esta configuração, acesse <https://myaccount.google.com/lesssecureapps>
  
  */
